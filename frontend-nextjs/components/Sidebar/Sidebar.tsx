1cdc428ef2d8cc0ef3d236dc2b3e7ebb3e2286cc
Anurag <anuragmarketing101@gmail.com>
2022-10-28 16:20:31 +0530

    initial-commit

---
+++ frontend-nextjs/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import {BsTwitter} from "react-icons/bs"
+import { BiBell , BiBookmark  } from "react-icons/bi"
+import { RiHashtag   } from "react-icons/ri"
+
+
+function Sidebar() {
+  return (
+    <div>
+        <BsTwitter className='text-blue-400 w-10 h-10' />
+
+        
+    </div>
+  )
+}
+
+export default Sidebar
